f7eacd94b24689816c48e87276b042513f416fbc
Juan P. Rodríguez B <jp.rodriguezb20@uniandes.edu.co>
2022-09-23 06:58:31 -0500

    Entities

---
+++ src/main/java/co/edu/uniandes/dse/parcialejemplo/entities/HotelEntity.java
@@ -0,0 +1,61 @@
+/*
+MIT License
+
+Copyright (c) 2021 Universidad de los Andes - ISIS2603
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package co.edu.uniandes.dse.parcialejemplo.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.Setter;
+import uk.co.jemos.podam.common.PodamExclude;
+
+/**
+ * Entidad genérica de la que heredan todas las entidades. Contiene la
+ * referencia al atributo id
+ *
+ * @author ISIS2603
+ */
+
+@Entity
+@Getter
+@Setter
+
+public class HotelEntity extends BaseEntity {
+	private String nombre;
+	private String direccion;
+	private Integer estrellas;
+
+	@PodamExclude
+	@OneToMany(mappedBy = "hotel", cascade = CascadeType.PERSIST, orphanRemoval = true);
+	private List<HabitacionEntity> habitaciones = new ArrayList<>();
+}
